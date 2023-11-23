6dd0ebbd170afac269fc2d65db75d9c32b8c2d49
w.tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2023-11-23 17:10:39 +0000

    Merged PR 1: Api versioning
    api versioning throught header added.
    
    Related work items: #5, #6

--- HR_Board/Program.cs
+++ HR_Board/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 namespace HR_Board
 {
     public class Program
@@ -11,6 +14,14 @@ namespace HR_Board
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddApiVersioning(o =>
+            {
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.ReportApiVersions = true;
+            });
+
+
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
