bde8f68379c03a478840e7da2f0b6255626c40be
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2023-11-28 16:47:53 +0100

    Identity configuration

---
+++ HR_Board/Data/HR_BoardDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Board.Data
+{
+    public class HR_BoardDbContext : IdentityDbContext<ApiUser>
+    {
+        public HR_BoardDbContext(DbContextOptions<HR_BoardDbContext> options) : base(options) 
+        { 
+        }
+    }
+}
--- HR_Board/Program.cs
+++ HR_Board/Program.cs
@@ -1,5 +1,8 @@
+﻿using HR_Board.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR_Board
 {
@@ -10,6 +13,23 @@ namespace HR_Board
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            // Add DbContext
+            builder.Services.AddDbContext<HR_BoardDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Konfiguracja Identity
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireLowercase = true; // Wymagana mała litera
+                options.Password.RequireUppercase = true; // Wymagana duża litera
+                options.Password.RequireDigit = true; // Wymagana cyfra
+                options.Password.RequireNonAlphanumeric = true; // Wymagany znak specjalny
+                options.Password.RequiredLength = 8; // Minimalna długość: 8 znaków
+            })
+                .AddEntityFrameworkStores<HR_BoardDbContext>()
+                .AddDefaultTokenProviders();
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -35,6 +55,7 @@ namespace HR_Board
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
