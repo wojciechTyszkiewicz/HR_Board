681d4753e7a8298d832f8f4d22ac9c047a66d8ab
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2023-12-29 12:01:51 +0100

    ILogger implementation

--- HR_Board/Controllers/UsersController.cs
+++ HR_Board/Controllers/UsersController.cs
@@ -31,7 +31,9 @@ namespace HR_Board.Controllers
         [HttpGet("GetUserClaims", Name = "GetUserClaims")]
         public IEnumerable<string> Get()
         {
-            return User.Claims.Select(x => $"{x.Type} -> {x.Value}").ToArray();
+            var claims = User.Claims.Select(x => $"{x.Type} -> {x.Value}").ToArray();
+            _logger.LogInformation("Retrieved {ClaimCount} claims for curent user.", claims.Length);
+            return claims;
         }
 
 
@@ -39,7 +41,12 @@ namespace HR_Board.Controllers
         [HttpGet("GetUser/{email}", Name = "GetUser")]
         public async Task<ApiUser?> GetUser(string email)
         {
+            _logger.LogInformation("Attempting to retrieve user with email: {Email}", email);
             var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                _logger.LogWarning("User not found with email: {Email}", email);
+            }
             return user;
         }
 
@@ -49,7 +56,22 @@ namespace HR_Board.Controllers
         public async Task<IdentityResult?> DeleteUser(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
-            return user != null ? await _userManager.DeleteAsync(user) : null;
+            if(user == null)
+            {
+                _logger.LogWarning("Attempted to delete non-existing user with e-mail: {Email}", email);
+                return null;
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if(result.Succeeded)
+            {
+                _logger.LogInformation("User with email: {Email} deleted succesfully.", email);
+            }
+            else
+            {
+                _logger.LogError("Error occured while deleting user with email: {Email}", email);
+            }
+            return result;
         }
 
 
@@ -58,6 +80,7 @@ namespace HR_Board.Controllers
         public async Task<ActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out succesfully.");
             return Ok("signed out");
         }
     }
