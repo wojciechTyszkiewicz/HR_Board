944ba1518d3d9797c4d1bc681a25cb0e214d2e46
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-01-07 15:10:31 +0100

    Identity configuration updated

--- HR_Board/Program.cs
+++ HR_Board/Program.cs
@@ -25,17 +25,7 @@ namespace HR_Board
 
             // Konfiguracja Identity
 
-            builder.Services.AddIdentityApiEndpoints<ApiUser>(options =>
-            {
-                options.Password.RequireLowercase = true; // Wymagana mała litera
-                options.Password.RequireUppercase = true; // Wymagana duża litera
-                options.Password.RequireDigit = true; // Wymagana cyfra
-                options.Password.RequireNonAlphanumeric = true; // Wymagany znak specjalny
-                options.Password.RequiredLength = 8; // Minimalna długość: 8 znaków
-
-            }).AddEntityFrameworkStores<AppDbContext>();
-
-            /*            builder.Services.AddIdentity<ApiUser, IdentityRole>(options =>
+            builder.Services.AddIdentity<ApiUser, IdentityRole>(options =>
             {
                 options.Password.RequireLowercase = true; // Wymagana mała litera
                 options.Password.RequireUppercase = true; // Wymagana duża litera
@@ -43,7 +33,7 @@ namespace HR_Board
                 options.Password.RequireNonAlphanumeric = true; // Wymagany znak specjalny
                 options.Password.RequiredLength = 8; // Minimalna długość: 8 znaków
             })
-                .AddDefaultTokenProviders();*/
+                .AddDefaultTokenProviders();
 
 
             builder.Services.AddControllers();
