98c9eddebf69a98387cd6a6b513918666df1444e
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-01-08 22:49:14 +0100

    savingChanges, savedChanges, and savchanges aplied

--- HR_Board/Data/ApiUser.cs
+++ HR_Board/Data/ApiUser.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HR_Board.Data.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace HR_Board.Data
 {
-    public class ApiUser : IdentityUser<Guid>
+    public class ApiUser : IdentityUser<Guid>, IBaseEntity
     {
-        public string? FirstName {  get; set; }
-        public string? LastName {  get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
--- HR_Board/Data/AppDbContext.cs
+++ HR_Board/Data/AppDbContext.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HR_Board.Data.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace HR_Board.Data
 {
@@ -8,33 +10,57 @@ namespace HR_Board.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+            SavingChanges += AppDbContext_SavingChanges;
+            SavingChanges += AppDbContext_SavedChanges;
+            SaveChangesFailed += AppDbContext_SaveChangesFailed;
         }
+
+        private void AppDbContext_SaveChangesFailed(object sender, SaveChangesFailedEventArgs e)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = true;
+        }
+
+        private void AppDbContext_SavedChanges(object sender, SavingChangesEventArgs e)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = true;
+        }
+
+        private void AppDbContext_SavingChanges(object sender, SavingChangesEventArgs e)
+        {
+            ChangeTracker.DetectChanges();
+            
+/*            var entities = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Modified);*/
+
+
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        entry.CurrentValues[nameof(IBaseEntity.IsDeleted)] = true;
+                        entry.State = EntityState.Modified;
+                        break;
+                    case EntityState.Modified:
+                        entry.CurrentValues[nameof(IBaseEntity.UpdatedAt)] = DateTime.UtcNow;
+                        break;
+                }
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             // Konfiguracja BaseEntity
-            modelBuilder.Entity<BaseEntity>()
-                .Property(b => b.Id)
-                .IsRequired()
-                .ValueGeneratedOnAdd();
-        }
-
-        public override int SaveChanges()
-        {
-            UpdateTimestamps();
-            return base.SaveChanges();
-        }
-
-        private void UpdateTimestamps()
-        {
-            var entities = ChangeTracker.Entries()
-                .Where(x => x.Entity is BaseEntity && x.State == EntityState.Modified);
-
-            foreach (var entity in entities)
+            modelBuilder.Entity<BaseEntity>(be =>
             {
-                ((BaseEntity)entity.Entity).UpdatedAt = DateTime.UtcNow;
-            }
+                be.HasQueryFilter(b => b.IsDeleted == false);
+                be.Property(b => b.Id).IsRequired().ValueGeneratedOnAdd();
+                be.Property(b => b.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+            });
+                
+                
         }
     }
 }
