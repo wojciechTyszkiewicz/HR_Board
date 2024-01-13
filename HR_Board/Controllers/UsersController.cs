621bd56e2490da37ff777f167de8d75724b2df72
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-01-13 17:53:19 +0100

    previous UserController deleted

--- HR_Board/Controllers/UsersController.cs
+++ HR_Board/Controllers/UsersController.cs
@@ -1,75 +1,27 @@
 ﻿using HR_Board.Data;
-using Microsoft.AspNetCore.Authorization;
+using HR_Board.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HR_Board.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class UsersController :ControllerBase
+    [Route("/api/[controller]")]
+
+    public class UsersController : ControllerBase
     {
-        private readonly ILogger<UsersController> _logger;
         private readonly UserManager<ApiUser> _userManager;
-        private readonly SignInManager<ApiUser> _signInManager;
-
-        public UsersController(
-            ILogger<UsersController> logger,
-            UserManager<ApiUser> userManager, 
-            SignInManager<ApiUser> signInManager)
-        {
-            _logger = logger;
+        private readonly AppDbContext _appDbContext;
+        private readonly JWTTokenService _tokenService;
+        public UsersController(UserManager<ApiUser> userManager, AppDbContext context, JWTTokenService tokenService) 
+        { 
             _userManager = userManager;
-            _signInManager = signInManager;
+            _appDbContext = context;
+            _tokenService = tokenService;
         }
 
 
 
-        [Authorize]
-        [HttpGet("GetUser/{email}", Name = "GetUser")]
-        public async Task<ApiUser?> GetUser(string email)
-        {
-            _logger.LogInformation("Attempting to retrieve user with email: {Email}", email);
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                _logger.LogWarning("User not found with email: {Email}", email);
-            }
-            return user;
-        }
 
-
-        [Authorize]
-        [HttpPost("DeleteUser/{email}", Name = "DeleteUser")]
-        public async Task<IdentityResult?> DeleteUser(string email)
-        {
-            var user = await _userManager.FindByEmailAsync(email);
-            if(user == null)
-            {
-                _logger.LogWarning("Attempted to delete non-existing user with e-mail: {Email}", email);
-                return null;
-            }
-
-            var result = await _userManager.DeleteAsync(user);
-            if(result.Succeeded)
-            {
-                _logger.LogInformation("User with email: {Email} deleted succesfully.", email);
-            }
-            else
-            {
-                _logger.LogError("Error occured while deleting user with email: {Email}", email);
-            }
-            return result;
-        }
-
-
-/*        [Authorize]
-        [HttpGet("/Logout", Name = "Logout")]
-        public async Task<ActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out succesfully.");
-            return Ok("signed out");
-        }*/
     }
 }
