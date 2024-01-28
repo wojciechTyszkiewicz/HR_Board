6e4f52a19c65831aa1f39802d015c8e3b25389c1
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-01-28 21:37:52 +0100

    UserDTO and RegistrationResponse dataAnnotation deleted

--- HR_Board/Models/DTO/UserDTO.cs
+++ HR_Board/Models/DTO/UserDTO.cs
@@ -10,7 +10,6 @@ namespace HR_Board.Models.DTO
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        [EmailAddress]
         public string Email { get; set; }
     }
 }
--- HR_Board/Services/Users/RegistrationResponse.cs
+++ HR_Board/Services/Users/RegistrationResponse.cs
@@ -15,7 +15,6 @@ namespace HR_Board.Services.Users
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        [EmailAddress]
         public string Email { get; set; }
 
 
