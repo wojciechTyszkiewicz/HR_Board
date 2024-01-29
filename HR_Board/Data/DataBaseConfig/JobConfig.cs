a40d85593ebc75eab055649119fb2b649737c6d4
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-01-29 19:34:35 +0100

    Job Fluent Api updated

--- HR_Board/Data/DataBaseConfig/JobConfig.cs
+++ HR_Board/Data/DataBaseConfig/JobConfig.cs
@@ -9,12 +9,12 @@ namespace HR_Board.Data.DataBaseConfig
         public override void Configure(EntityTypeBuilder<Job> builder)
         {
             base.Configure(builder);
-
+/*
             builder.Property(j => j.Title).HasMaxLength(100);
             builder.Property(j => j.ShortDescription).HasMaxLength(500);
             builder.Property(j => j.LongDescription).HasMaxLength(2000);
             builder.Property(j => j.Logo).HasMaxLength(256);
-            builder.Property(j => j.CompanyName).HasMaxLength(100);
+            builder.Property(j => j.CompanyName).HasMaxLength(100);*/
 
             builder.HasOne<ApiUser>()
            .WithMany()
