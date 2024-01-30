7904eaf7efaf6a156dc6120384f6380d496415f1
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-01-30 13:31:04 +0100

    JobConfig updated

--- HR_Board/Data/DataBaseConfig/JobConfig.cs
+++ HR_Board/Data/DataBaseConfig/JobConfig.cs
@@ -9,12 +9,11 @@ namespace HR_Board.Data.DataBaseConfig
         public override void Configure(EntityTypeBuilder<Job> builder)
         {
             base.Configure(builder);
-/*
-            builder.Property(j => j.Title).HasMaxLength(100);
-            builder.Property(j => j.ShortDescription).HasMaxLength(500);
-            builder.Property(j => j.LongDescription).HasMaxLength(2000);
-            builder.Property(j => j.Logo).HasMaxLength(256);
-            builder.Property(j => j.CompanyName).HasMaxLength(100);*/
+
+            builder.Property(j => j.Title).HasMaxLength(50);
+            builder.Property(j => j.ShortDescription).HasMaxLength(15);
+            builder.Property(j => j.LongDescription).HasMaxLength(250);
+            builder.Property(j => j.CompanyName).HasMaxLength(50);
 
             builder.HasOne<ApiUser>()
            .WithMany()
--- HR_Board/Data/Entities/Job.cs
+++ HR_Board/Data/Entities/Job.cs
@@ -5,19 +5,15 @@ namespace HR_Board.Data.Entities
 {
     public class Job : BaseEntity
     {
-        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [StringLength(15, MinimumLength = 5)]
         public string ShortDescription { get; set; }
 
-        [StringLength(250, MinimumLength = 5)]
         public string LongDescription { get; set; }
 
         [Url]
         public string Logo { get; set; }
 
-        [StringLength(50, MinimumLength = 5)]
         public string CompanyName { get; set; }
 
         public JobStatus Status { get; set; }
