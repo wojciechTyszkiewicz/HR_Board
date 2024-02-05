6215d63f3180c526b1d01a938d9d786bfb13673b
Wojciech Tyszkiewicz <w.tyszkiewicz@areaprojekt.pl>
2024-02-05 09:34:40 +0100

    job controller

---
+++ HR_Board/Controllers/JobController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR_Board.Data;
+using HR_Board.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public JobController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Job
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Job>>> GetJobs()
+        {
+            return await _context.Jobs.ToListAsync();
+        }
+
+        // GET: api/Job/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return job;
+        }
+
+        // POST: api/Job
+        [HttpPost]
+        public async Task<ActionResult<Job>> PostJob(Job job)
+        {
+            _context.Jobs.Add(job);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetJob), new { id = job.Id }, job);
+        }
+
+        // PUT: api/Job/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(Guid id, Job job)
+        {
+            if (id != job.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(job).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Job/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
